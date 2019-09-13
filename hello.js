51990edaad7c8e39624da3df4bb371d0849dd273
thislooksfun <thislooksfun@users.noreply.github.com>
2019-09-13 03:18:24 -0500

    Update hello.js

--- hello.js
+++ hello.js
@@ -18,6 +18,7 @@ async function getAllLinesCommentedOnByBot(context) {
   // ok seriously
   // where is this bug??
   // ...found it...
+  // wheeeee
   return context.github.paginate(
     // context.issue() provides owner, repo, and number
     context.github.pullRequests.listComments(context.issue({ per_page: 100 })),
